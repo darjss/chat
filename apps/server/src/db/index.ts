156900708e90b09769f57af797bc98fd7dc8cf0d
darjss <darjsavid@gmail.com>
2025-05-10 12:56:21 +0800

    deployed to workers

--- apps/server/src/db/index.ts
+++ apps/server/src/db/index.ts
@@ -2,8 +2,8 @@ import { drizzle } from "drizzle-orm/libsql";
 import { createClient } from "@libsql/client";
 
 const client = createClient({
-  url: process.env.DATABASE_URL || "",
-  authToken: process.env.DATABASE_AUTH_TOKEN ,
+  url: "libsql://chat-darjss.aws-ap-northeast-1.turso.io",
+  authToken: "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3NDY4NTIxMTQsImlkIjoiMzZiNzdlOWYtZjk3MS00ODA2LWE2MmQtZDgyNTdhMTdlZDE2IiwicmlkIjoiNTU3YzQ1ZjctYmJjOC00NDRjLWE5ZTYtNWFmZWMwODRlNDAzIn0.6oWgV4SPscetfLF--Ew8yBQDDAZ-V-f8t4twF_GUBYTcTu1SA4NB21XNLUrnUnR2_sQuCVqKyoJmV6YtSywaCg",
 });
 
 export const db = drizzle({ client });
